528f9433f3f9eed1765cbf2bc3a1265abd8a544b
Diego Osvaldo <d.cruz@outlook.com>
2020-04-29 12:23:56 -0400

    Edit view for title

--- webplayground/core/views.py
+++ webplayground/core/views.py
@@ -6,15 +6,9 @@ from django.views.generic.base import TemplateView
 
 class HomePageView(TemplateView):
     template_name = "core/home.html"
-
-    def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)
-        # context['latest_articles'] = Article.objects.all()[:5]
-        context['title'] = 'Mi super web Playground desde context'
-        return context
     
     def get(self, request, *args, **kwargs):
-        return render(request, self.template_name, {'title': 'Mi super web Playground desde GET'})
+        return render(request, self.template_name, {'title': 'Mi Web App Playground con Django 2.2'})
 
 class SamplePageView(TemplateView):
     template_name = "core/sample.html"
